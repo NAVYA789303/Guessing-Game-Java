700b55bcd8326a5bf03c22b0b8f72cd9f4d3aefd
NAVYA789303 <navyaesampally575@gmail.com>
2026-07-08 17:04:00 +0530

    Rename class guessinggame to Guessinggame

--- guessinggame.java
+++ guessinggame.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class guessinggame {
+public class Guessinggame {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int k=5;
