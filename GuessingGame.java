043b0eea456d53d7fe7a465b82a670f382061ce4
NAVYA789303 <navyaesampally575@gmail.com>
2026-07-08 17:07:01 +0530

    Fix class name casing from 'Guessinggame' to 'GuessingGame'

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Guessinggame {
+public class GuessingGame {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int k=5;
